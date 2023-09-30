179c51e1421a1a9bd3adbd4f2fcd9a5ecbf2600f
alpin <alpinagung1@gmail.com>
2023-09-30 11:01:12 +0700

    me-alpin[update]

--- lib/core/assets/app_assets.dart
+++ lib/core/assets/app_assets.dart
@@ -3,6 +3,6 @@ class AppAssets {
   static const svg = '${baseURL}svg/';
   static const animation = '${baseURL}animation/';
   static const image = 'img/'; // JPG or PNG
-
+  static const bgnew = '${image}bgnew.png';
   static const logoPiksi = '${image}logo_piksi.png';
 }
